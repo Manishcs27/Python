5dca996a3634934d01c6da1f1dcc0a811e383cec
Manishcs27 <153353639+Manishcs27@users.noreply.github.com>
2026-09-06 21:30:42 +0530

    Learn about loops

---
+++ loop.py
@@ -0,0 +1,36 @@
+exp = [2340,2500,2100,3100,2980]
+# total = exp[0] + exp[1] + exp[2] + exp[3] + exp[4]
+# print(total)
+
+total =0
+# for item in exp:
+#     total = total+item
+# print(total)
+
+# for i in range(len(exp)):
+#     print("Month",(i+1),"Expense",exp[i])
+#     total = total+exp[i]
+# print("Total",total)
+
+# key_location = "chair"
+# locations = ["garage","living room","chair","closet"]
+# for i in locations:
+#
+#     if i==key_location:
+#         print("key found in",i)
+#         break
+#     else:
+#        print("key not found in",i)
+
+
+
+# for i in range(1,6):
+#     if i % 2 == 0:
+#         continue
+#     print(i*i)
+
+
+# i=1
+# while i<=5:
+#     print(i)
+#     i=i+1
