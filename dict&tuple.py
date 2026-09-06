8b273d5b07bc56e3b0dd272aeab886e74e1dc2ea
Manishcs27 <153353639+Manishcs27@users.noreply.github.com>
2026-09-06 22:06:52 +0530

    Learn about dict and tuples

---
+++ dict&tuple.py
@@ -0,0 +1,30 @@
+d={"tom":2456789890,"Alex":7325730239,"Joe":"8434209987"}
+
+# print(d["tom"])
+# d["sam"]=7766767674
+# print(d)
+# del d["sam"]
+# print(d)
+#
+# for key in d:
+#     print("key is:",key,"value is:",d[key])
+#
+# for k,v in d.items():
+#     print("key is:",k,"value is:",v)
+
+
+
+
+
+print("tom" in d)
+
+d.clear()
+print(d)
+
+
+
+
+point = (5,9)
+print(point[0])
+
+
