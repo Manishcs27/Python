5588687b49b01cdc0ee84f3c8a24edbcb3e11217
Manishcs27 <153353639+Manishcs27@users.noreply.github.com>
2026-09-07 22:51:34 +0530

    Learn about modules

---
+++ module.py
@@ -0,0 +1,14 @@
+# import math
+# print(math.ceil(math.log(10,2)))
+# print(math.floor(math.log(10,2)))
+# print(math.sqrt(10))
+# print(math.pow(10,2))
+#
+
+# import calendar
+# cal = calendar.calendar(2026,11)
+# print(cal)
+# cal = calendar.month(2026,11)
+# print(cal)
+
+
